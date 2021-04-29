5ab8f96bd32b758cf538ff5e87c30ebbaed9d646
AntoniaDeville <deville.antonia@laposte.net>
2021-04-29 10:15:08 +0200

    ajout java

---
+++ javahelloworld.java
@@ -0,0 +1,29 @@
+FROM java:_
+
+ 
+
+RUN apt-get update
+
+ 
+
+RUN apt-get install -y wget
+
+ 
+
+RUN apt-get install -y iputils-ping
+
+ 
+
+#CMD ["ping","127.0.1","-c","30"]
+
+ 
+
+#ENTRYPOINT ["ping","-c","10"]
+#CMD ["127.0.0.2"]
+
+COPY testfile.txt /tmp/testfile.txt
+
+ENV testVariable toto
+ 
+ENTRYPOINT ["java", "HelloWorld"]
+
